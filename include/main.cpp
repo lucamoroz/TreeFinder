3097836d49017ce95aab64b62109069909643eae
Luca <lucamoro96@gmail.com>
2020-07-09 14:15:26 +0200

    added testing method

--- include/main.cpp
+++ include/main.cpp
@@ -3,6 +3,25 @@
 
 using namespace std;
 
+void runAndSaveResults() {
+    TreeFinder treeFinder = TreeFinder::loadTreeFinder();
+
+    vector<String> images_path;
+    glob("../data/images/test/*.*",images_path);
+
+    for (int i = 0; i < images_path.size(); i++) {
+        cout << "analyzing " << images_path[i] << endl;
+        Mat img = imread(images_path[i]);
+
+        vector<Rect2i> locations = treeFinder.locateTrees(img, DEFAULT_MIN_CONF);
+
+        for (int j = 0; j < locations.size(); j++) {
+            rectangle(img, locations[j], Scalar(250,255,0), 3);
+        }
+
+        imwrite("../results/" + to_string(i) + ".jpg", img);
+    }
+}
 
 int main(int argc, char* argv[]) {
     TreeFinder treeFinder = TreeFinder::loadTreeFinder();
@@ -14,6 +33,10 @@ int main(int argc, char* argv[]) {
     } else {
         cout << "Success loading TreeFinder" << endl;
     }
+
+
+    runAndSaveResults();
+    return 0;
 
     if (argc < 2 || argc > 3) {
         cout << "USAGE: ./TreeFinder IMG_PATH [OPT - MIN_CONF]" << endl;
@@ -27,7 +50,6 @@ int main(int argc, char* argv[]) {
         min_conf = stof(argv[2]);
 
     Mat img = imread(img_path);
-    // GaussianBlur(img, img, Size(3,3), 0.4, 0.4);
 
     vector<Rect2i> locations = treeFinder.locateTrees(img, min_conf);
 
